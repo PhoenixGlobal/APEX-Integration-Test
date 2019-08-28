5134170d658e145a95ba591e9fecca780bb0a505
Artem <ae@atr0x.com>
2019-08-28 20:16:27 +0200

    add wait for running chain

--- src/main/java/test/MainTestRunner.java
+++ src/main/java/test/MainTestRunner.java
@@ -1,5 +1,6 @@
 package test;
 
+import message.request.cmd.GetBlockByHeightCmd;
 import message.util.RequestCallerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,7 +14,7 @@ public class MainTestRunner {
 
     private static final Logger LOG = LoggerFactory.getLogger(MainTestRunner.class);
 
-    public static void main (String [] args) {
+    public static void main (String [] args) throws Exception {
 
         final String privKeySender = args[0];
         final String addressReceiver = args[1];
@@ -25,11 +26,21 @@ public class MainTestRunner {
 
         testsToExecute.forEach(test -> results.add(test.executeTest(callerService, rpcUrl)));
 
+        boolean chainIsNotProducing = true;
+
+        GetBlockByHeightCmd cmd = new GetBlockByHeightCmd(12);
+        while (chainIsNotProducing){
+            String response = callerService.postRequest(rpcUrl, cmd);
+            if(response.contains("\"status\":200") && response.contains("\"succeed\":true")){
+                chainIsNotProducing = false;
+            } else {
+                Thread.sleep(10000L);
+            }
+        }
+
         LOG.info("----------------------------- TEST FINISHED --------------------------------------------");
         LOG.info("-----------------------------               --------------------------------------------");
         LOG.info("-----------------------------               --------------------------------------------");
-        int success = 0;
-        int fail = 0;
         results.forEach(result -> {
             if(result.get("status").equals("error")){
                 LOG.error("Name: " + result.get("name") + "               Status: " + result.get("status"));
